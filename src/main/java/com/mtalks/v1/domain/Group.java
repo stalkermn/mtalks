00f03b8d8db26d96dfab75a6319f877e0b698f4c
stalkermn <valerij.vasilkov@gmail.com>
2013-04-02 19:18:39 +0300

    fix tests and mail sender, add test db with name mTalks-test to test context,

---
+++ src/main/java/com/mtalks/v1/domain/Group.java
@@ -0,0 +1,19 @@
+package com.mtalks.v1.domain;
+
+import java.util.Set;
+
+/**
+ * User: vvasil
+ * Date: 3/31/13
+ * Time: 12:49 AM
+ */
+public class Group {
+
+    private String id;
+    private String name;
+    private Set<String> members;
+    private Set<String> genres;
+    private Set<String> songs;
+
+
+}
---
+++ src/main/java/com/mtalks/v1/domain/Wall.java
@@ -0,0 +1,9 @@
+package com.mtalks.v1.domain;
+
+/**
+ * User: vvasssil
+ * Date: 3/31/13
+ * Time: 12:51 AM
+ */
+public class Wall {
+}
